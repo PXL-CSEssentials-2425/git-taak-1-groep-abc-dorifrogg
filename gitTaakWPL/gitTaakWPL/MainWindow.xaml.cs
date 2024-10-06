d0c9d637ea3df00965088acadfffc7b9707f3bf3
dorifrogg <12104047@student.pxl.be>
2024-10-06 22:39:00 +0200

    KLEUR-05 Informatie

--- gitTaakWPL/gitTaakWPL/MainWindow.xaml.cs
+++ gitTaakWPL/gitTaakWPL/MainWindow.xaml.cs
@@ -44,41 +44,53 @@ namespace gitTaakWPL
         private void imageRood_MouseLeave(object sender, MouseEventArgs e)
         {
             gridMainWindow.Background = Brushes.LightGray;
+            kleurcodeTextBox.Text = "";
+            informatieTextBox.Text = "";
         }
 
         private void imageGeel_MouseLeave(object sender, MouseEventArgs e)
         {
             gridMainWindow.Background = Brushes.LightGray;
+            kleurcodeTextBox.Text = "";
+            informatieTextBox.Text = "";
         }
 
         private void imageGroen_MouseLeave(object sender, MouseEventArgs e)
         {
             gridMainWindow.Background = Brushes.LightGray;
+            kleurcodeTextBox.Text = "";
+            informatieTextBox.Text = "";
         }
 
         private void imageBlauw_MouseLeave(object sender, MouseEventArgs e)
         {
             gridMainWindow.Background = Brushes.LightGray;
+            kleurcodeTextBox.Text = "";
+            informatieTextBox.Text = "";
         }
 
         private void imageRood_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             kleurcodeTextBox.Text = "#FF0000";
+            informatieTextBox.Text = "Rood is de kleur van warmte";
         }
 
         private void imageGeel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             kleurcodeTextBox.Text = "#FFFF00";
+            informatieTextBox.Text = "Geel is de kleur van levenslust";
         }
 
         private void imageGroen_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             kleurcodeTextBox.Text = "#008000";
+            informatieTextBox.Text = "Groen is de kleur van genezing";
         }
 
         private void imageBlauw_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             kleurcodeTextBox.Text = "#0000FF";
+            informatieTextBox.Text = "Blauw is de kleur van intelligentie";
         }
     }
 }
